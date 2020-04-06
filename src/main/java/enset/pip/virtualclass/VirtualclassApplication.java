a9db17648d8d3f1e8acf2fda252e9611243c1a21
Kharrazi yahya <kharrazi1031yahya@gmail.com>
2020-04-06 19:14:38 +0100

    Initial commit

---
+++ src/main/java/enset/pip/virtualclass/VirtualclassApplication.java
@@ -0,0 +1,13 @@
+package enset.pip.virtualclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/enset/pip/virtualclass/VirtualclassApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.pip.virtualclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
