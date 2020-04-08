f270390c58b4fc1f8f62c018668a9400f7f3d390
AyoubBBK <56084983+AyoubBBK@users.noreply.github.com>
2020-04-08 23:06:40 +0100

    Fixed the missing attributes of the NoteTacheCleBean

--- src/main/java/enset/pip/virtualclass/entities/NoteTacheCle.java
+++ src/main/java/enset/pip/virtualclass/entities/NoteTacheCle.java
@@ -1,4 +1,23 @@
 package enset.pip.virtualclass.entities;
 
-public class NoteTacheCle {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class NoteTacheCle implements Serializable {
+    @Column(name = "tache_id")
+    Long tacheId;
+
+    @Column(name = "groupe_id")
+    String groupeId;
 }
