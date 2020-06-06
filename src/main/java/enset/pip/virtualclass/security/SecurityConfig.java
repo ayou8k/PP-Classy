beb076a1c988239fd147458815f426ef23ddfcdc
Kharrazi yahya <kharrazi1031yahya@gmail.com>
2020-06-06 21:41:57 +0100

    init security

---
+++ src/main/java/enset/pip/virtualclass/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package enset.pip.virtualclass.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user1").password("{noop}1234").roles("USER");
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select login as principal,password as credentials from compte where login=?")
+                .authoritiesByUsernameQuery("select from roles");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http.formLogin();
+//        http.httpBasic();
+
+        //all requests to be executed the user must be loggedIn
+        http.authorizeRequests().anyRequest().authenticated();
+        http.csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
